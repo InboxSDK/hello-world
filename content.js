f1ac5cdf9832f6ab67763de9d12c80f1737288ba
aleemstreak <aleem@streak.com>
2015-02-13 16:04:27 -0800

    Update content.js

--- content.js
+++ content.js
@@ -6,7 +6,7 @@ InboxSDK.load('1', 'Hello World!').then(function(sdk){
 		// a compose view has come into existence, do something with it!
 		composeView.addButton({
 			title: "My Nifty Button!",
-			iconUrl: 'https://www.streak.com/images/logoLight.png',
+			iconUrl: 'https://lh5.googleusercontent.com/itq66nh65lfCick8cJ-OPuqZ8OUDTIxjCc25dkc4WUT1JG8XG3z6-eboCu63_uDXSqMnLRdlvQ=s128-h128-e365',
 			onClick: function(event) {
 				event.composeView.insertTextIntoBodyAtCursor('Hello World!');
 			},
